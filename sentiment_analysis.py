aee2882db1775dbcfaa210cc8f3bc71e22aa64d4
janische <55109259+janische@users.noreply.github.com>
2019-09-27 20:46:58 -0400

    Google code that now processes Twitter inputs

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -23,50 +23,37 @@ from google.cloud.language import types
 # [END language_sentiment_tutorial_imports]
 
 
-def average(sentiments):
-    num_Tweets = [0, 0, 0]
-    total_score = [0, 0, 0]
-    total_magnitude = [0, 0, 0]
-    sentiment_avgs = [0, 0, 0]
-    magnitude_avgs = [0, 0, 0]
+def average(sentiments,num_Tweets,total_score,total_magnitude,sentiment_avgs,magnitude_avgs):
+    score = sentiments.document_sentiment.score
+    magnitude = sentiments.document_sentiment.magnitude
+    if (score >= 0.25): # positive sentiment
+        num_Tweets[0] = num_Tweets[0] + 1
+        total_score[0] = total_score[0] + score
+        total_magnitude[0] = total_magnitude[0] + magnitude
+    elif (score <= -0.25): # negative sentiment
+        num_Tweets[1] = num_Tweets[1] + 1
+        total_score[1] = total_score[1] + score
+        total_magnitude[1] = total_magnitude[1] + magnitude
+    else:                               # neutral sentiment
+        num_Tweets[2]= num_Tweets[2] + 1
+        total_score[2] = total_score[2] + score
+        total_magnitude[2] = total_magnitude[2] + magnitude
 
-    for index, sentence in enumerate(sentiments.sentences):
-        sentence_sentiment = sentence.sentiment.score
-        sentence_magnitude = sentence.sentiment.magnitude
-        if (sentence_sentiment >= 0.25): # positive sentiment
-            num_Tweets[0] = num_Tweets[0] + 1
-            total_score[0] = total_score[0] + sentence_sentiment
-            total_magnitude[0] = total_magnitude[0] + sentence_magnitude
-        elif (sentence_sentiment <= -0.25): # negative sentiment
-            num_Tweets[1] = num_Tweets[1] + 1
-            total_score[1] = total_score[1] + sentence_sentiment
-            total_magnitude[1] = total_magnitude[1] + sentence_magnitude
-        else:                               # neutral sentiment
-            num_Tweets[2]= num_Tweets[2] + 1
-            total_score[2] = total_score[2] + sentence_sentiment
-            total_magnitude[2] = total_magnitude[2] + sentence_magnitude
-    for it in range(0,3):
-        if (num_Tweets[it] != 0):
-            sentiment_avgs[it] = total_score[it]/num_Tweets[it]
-            magnitude_avgs[it] = total_magnitude[it]/num_Tweets[it]
-
-    return sentiment_avgs,magnitude_avgs,num_Tweets
-
-
-def print_result(results):
-    sentiment_avgs,magnitude_avgs,num_Tweets = results
+def print_result(sentiment_avgs,magnitude_avgs,num_Tweets):
     percent_pos = 100.00*num_Tweets[0]/sum(num_Tweets)
     percent_neg = 100.00*num_Tweets[1]/sum(num_Tweets)
     percent_neu = 100.00*num_Tweets[2]/sum(num_Tweets)
     
-    print('The results were {}% positive, {}% negative, and {}% neutral.'.format(
+    print("\n\n\n")
+    print("Out of {} tweets in supported languages, the results were:".format(sum(num_Tweets)))
+    print(' {:.2f}% positive, {:.2f}% negative, and {:.2f}% neutral.'.format(
             percent_pos, percent_neg, percent_neu))
 
-    print('Averages for Positive Results: \n Score of {:.2f} with magnitude of {:.2f}'.format(
+    print('\nAverages for Positive Results: \n  Score of {:.2f} with magnitude of {:.2f}'.format(
         sentiment_avgs[0], magnitude_avgs[0]))
-    print('Averages for Negative Results: \n Score of {:.2f} with magnitude of {:.2f}'.format(
+    print('Averages for Negative Results: \n  Score of {:.2f} with magnitude of {:.2f}'.format(
         sentiment_avgs[1], magnitude_avgs[1]))
-    print('Averages for Neutral Results: \n Score of {:.2f} with magnitude of {:.2f}'.format(
+    print('Averages for Neutral Results: \n  Score of {:.2f} with magnitude of {:.2f}'.format(
         sentiment_avgs[2], magnitude_avgs[2]))
     
     return 0
@@ -74,20 +61,39 @@ def print_result(results):
 
 # [START language_sentiment_tutorial_analyze_sentiment]
 def analyze(input_filename):
+    num_Tweets = [0, 0, 0]
+    total_score = [0, 0, 0]
+    total_magnitude = [0, 0, 0]
+    sentiment_avgs = [0, 0, 0]
+    magnitude_avgs = [0, 0, 0]
     """Run a sentiment analysis request on text within a passed filename."""
     client = language.LanguageServiceClient()
 
-    with open(input_filename, 'r') as review_file:
+    with open(input_filename, 'r') as tweets_file:
         # Instantiates a plain text document.
-        content = review_file.read()
+        content = tweets_file.read()
+    start_index = 0
+    end_index = 0
+    while (True):
+        end_index = content.find("This tweet ends.",start_index)
+        if end_index == -1:
+            break
+        tweet = content[start_index:end_index]
+        start_index = end_index + len("This tweet ends.")
+        document = types.Document(
+            content=tweet,
+            type=enums.Document.Type.PLAIN_TEXT)
+        try:
+            sentiments = client.analyze_sentiment(document=document)
+        except:
+            continue
+        average(sentiments,num_Tweets,total_score,total_magnitude,sentiment_avgs,magnitude_avgs)
+    for it in range(0,3):
+        if (num_Tweets[it] != 0):
+            sentiment_avgs[it] = total_score[it]/num_Tweets[it]
+            magnitude_avgs[it] = total_magnitude[it]/num_Tweets[it]
 
-    document = types.Document(
-        content=content,
-        type=enums.Document.Type.PLAIN_TEXT)
-    sentiments = client.analyze_sentiment(document=document)
-
-    sentiment_results = average(sentiments)
-    print_result(sentiment_results)
+    print_result(sentiment_avgs,magnitude_avgs,num_Tweets)
 # [END language_sentiment_tutorial_analyze_sentiment]
 
 if __name__ == '__main__':
